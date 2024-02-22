d21b0b667b35608e19a48b2c70dc5176dea84023
yatharth-gupta <guptayatharth1@gmail.com>
2024-02-22 18:50:13 +0530

    Fix design smells

--- FacebookUser.java
+++ FacebookUser.java
@@ -1,36 +1,33 @@
-package com.sismics.books.rest.model;
-
-/**
- * Facebook test user.
- * 
- * Management : https://developers.facebook.com/apps/387037158089019/roles?role=test%20users
- * 
- * @author jtremeaux
- */
+java
 public class FacebookUser {
-    public String id;
-    
-    public String email;
-    
-    public String fullName;
+    private String id;
+    private String email;
+    private String fullName;
+    private String accessToken;
 
-    public String accessToken;
-    
     public FacebookUser(String id, String email, String fullName, String accessToken) {
+        if (id == null || email == null || fullName == null || accessToken == null) {
+            throw new IllegalArgumentException("All arguments must be non-null");
+        }
         this.id = id;
         this.email = email;
-        this.accessToken = accessToken;
         this.fullName = fullName;
+        this.accessToken = accessToken;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
     }
 }
-
-
-# fixed design smells
-
-# fixed design smells
-
-// fixed design smells
-
-// fixed design smells
-
-// fixed design smells
