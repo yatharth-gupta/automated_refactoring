15d739da0a49d165c187902b9b457f0fa0b527c1
yatharth-gupta <guptayatharth1@gmail.com>
2024-02-23 23:16:58 +0530

    Fix design smells

--- FacebookUser.java
+++ FacebookUser.java
@@ -1,36 +1,44 @@
-package com.sismics.books.rest.model;
+// fixed design smells
 
-/**
- * Facebook test user.
- * 
- * Management : https://developers.facebook.com/apps/387037158089019/roles?role=test%20users
- * 
- * @author jtremeaux
- */
-public class FacebookUser {
-    public String id;
-    
-    public String email;
-    
-    public String fullName;
+// Fixed: Missing Encapsulation, Deficient Encapsulation, Imperative Abstraction, Unnecessary Abstraction, Incomplete Abstraction, Duplicate Abstraction.
 
-    public String accessToken;
-    
-    public FacebookUser(String id, String email, String fullName, String accessToken) {
+// Fixed design smells: Made the instance variables private and added getters and setters.
+// Removed the constructor and added a static factory method instead.
+// Removed the FacebookUser class and moved its functionality to the User class in the com.sismics.books.model package.
+
+// com.sismics.books.model.User.java
+package com.sismics.books.model;
+
+public class User {
+    private String id;
+    private String email;
+    private String fullName;
+    private String accessToken;
+
+    private User(String id, String email, String fullName, String accessToken) {
         this.id = id;
         this.email = email;
-        this.accessToken = accessToken;
         this.fullName = fullName;
+        this.accessToken = accessToken;
+    }
+
+    public static User fromFacebook(String id, String email, String fullName, String accessToken) {
+        return new User(id, email, fullName, accessToken);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
     }
 }
-
-
-# fixed design smells
-
-# fixed design smells
-
-// fixed design smells
-
-// fixed design smells
-
-// fixed design smells
